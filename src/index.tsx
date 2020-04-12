3d920e07b637b44092e2e2c3ef20b7c32954ddfe
yapi <yapi.tw@gmail.com>
2020-04-12 20:53:41 +0800

    upated styles

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,17 @@ import ThreeContainer from './components/ThreeContainer';
 import MenuList from './components/MenuList';
 
 const App = () => {
+
+  const [isMenuShow, setIsMenuShow] = React.useState(false)
+
   return (
     <HashRouter hashType="noslash">
       <div className="app">
-        <div className="menu-list">
-          <MenuList />
+        <div className={`menu-list ${isMenuShow ? "menu-list--active" : ""}`}>
+          <div className="switch" onClick={() => setIsMenuShow(!isMenuShow)}></div>
+          <div className="list-wrapper" onClick={() => setIsMenuShow(false)}>
+            <MenuList />
+          </div>
         </div>
         <div className="content">
           <Route path="/:id" component={ThreeContainer} />
